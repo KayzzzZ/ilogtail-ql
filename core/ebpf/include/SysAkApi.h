5d2b1b662ac3f49d4c0d93c1203c6c11d7f4b201
qianlu.kk <qianlu.lk@alibaba-inc.com>
2025-01-08 09:44:42 +0800

    network observer start done ..

--- core/ebpf/include/SysAkApi.h
+++ core/ebpf/include/SysAkApi.h
@@ -6,8 +6,8 @@
 
 #include "ebpf/include/export.h"
 
-using init_func = int (*)(nami::eBPFConfig*);
-using remove_func = int (*)(nami::eBPFConfig*);
-using deinit_func = void (*)(void);
-using suspend_func = int (*)(nami::eBPFConfig*);
-using update_func = int (*)(nami::eBPFConfig*);
+// using init_func = int (*)(logtail::ebpf::eBPFConfig*);
+// using remove_func = int (*)(logtail::ebpf::eBPFConfig*);
+// using deinit_func = void (*)(void);
+// using suspend_func = int (*)(logtail::ebpf::eBPFConfig*);
+// using update_func = int (*)(logtail::ebpf::eBPFConfig*);
